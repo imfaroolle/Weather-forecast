47832ae2328835be6d64ded854f923b315bd4a7e
ahmed-Faroolle <axmedfaroolle@gmail.com>
2020-02-23 09:47:53 +0300

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+console.log("browser window log");
+
+const form = document.querySelector('form');
+const search = document.querySelector('input');
+const message2 = document.querySelector('#message2');
+const message1 = document.querySelector('#message1');
+form.addEventListener('submit' , (e) =>{
+    e.preventDefault();
+    const lc = search.value;
+
+    if(lc == ''){
+
+        message2.textContent = "Location must be provided";
+    }
+    message2.textContent = "";
+    fetch('http://localhost:3000/weather?address=' + lc).then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            console.log(data.error);
+            message1.textContent = data.error;
+        }
+        else{
+            
+            console.log(data.location)
+            console.log(data.forecast)
+            message1.textContent = "Location : " + data.location;
+            message2.textContent = " Forecast : " + data.forecast;
+        }
+
+    })
+})
+
+
+})
